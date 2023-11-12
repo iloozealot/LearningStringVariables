9c5f836b6678c048126801a793a444b8099a19dc
iloozealot <iloozealot@gmail.com>
2023-11-11 21:34:02 -0600

    Add project files.

---
+++ LearningStringVariables/Program.cs
@@ -0,0 +1,17 @@
+﻿
+
+string firstName = string.Empty;
+string lastName = string.Empty;
+string filePath = string.Empty;
+
+firstName = "Justin";
+lastName = "Spencer";
+//filePath = "C:\\Temp\\Demo";
+filePath = @"C:\Temp\Demo";
+//firstName = "123";
+
+string testString = $@"The file for {firstName} is at C:\SampleFiles";
+
+Console.WriteLine($"Hello {firstName} {lastName}");
+Console.WriteLine(filePath);
+Console.WriteLine(testString);
